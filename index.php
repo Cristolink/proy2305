995dae09dd811b4ecdb3a4af2fa0a94c694c9abd
cetecom <cetecom@LC08L5727>
2017-05-23 10:59:38 -0400

    xdddd

--- index.php
+++ index.php
@@ -10,8 +10,24 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div>
+            <?php if(isset($_SESSION['USR'])) { ?>
+            <A href="cerrar.php">Cerrar Sesión</a>
+            <?php }
+            ?>
+        </div>
+        
+     
+          <?php if (!isset($_SESSION['USR'])){ ?>
+          <form action="revision.php" method="POST">
+              <table border="3"><tr><td>Usuario</td><td>Contraseña</td></tr>
+                  <tr><td><input type="text" name="nombre"/></td><td><input type="paswword" name="clave"/></td></tr>
+                  <tr rowspan="2"><td rowspan="2"> <input type="submit" value="Acceder"></td></tr>
+              </table>
+              
+              
+             
+          </form>
+          <?php }?>
     </body>
 </html>
